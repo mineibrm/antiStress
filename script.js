c013cb8c1e60c8f4af67ac0f236c7b6e370a2b1a
mineibrm <52788436+mineibrm@users.noreply.github.com>
2024-04-25 13:50:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,4 @@ function getCycleTime() {
 setInterval(() => {
     const timeInSeconds = getCycleTime();
     updateInstruction(timeInSeconds);
-}, 500); // Use a smaller interval for more accurate updates
+}, 1); // Use a smaller interval for more accurate updates
